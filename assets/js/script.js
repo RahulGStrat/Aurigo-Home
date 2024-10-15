f2fca6f2d224d4e9a82b566adfb53db2e0f218b6
RahulGStra <rahul.g@strat-agile.com>
2024-10-15 13:30:12 +0530

    updated

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// slick slider for banner
+$('.banner-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
